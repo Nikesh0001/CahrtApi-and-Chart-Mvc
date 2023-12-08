9e22115e103f118bd6f05c3523ff6c79acab47be
nikesh <knikesh5601@gmail.com>
2023-12-08 12:18:49 +0530

    Added

---
+++ ChartDAL/DataModels/ChartDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartDAL.DataModels
+{
+    public class ChartDbContext : DbContext
+    {
+        public ChartDbContext(DbContextOptions<ChartDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<EntityTbl> Entities { get; set; }
+        public DbSet<Feature> Features { get; set; }
+
+    }
+}
---
+++ ChartDAL/Repository/ChartRepository.cs
@@ -0,0 +1,41 @@
+﻿using ChartDAL.DataModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartDAL.Repository
+{
+    public class ChartRepository : IRepository<Feature>
+    {
+        ChartDbContext context;
+        public ChartRepository(ChartDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Feature> GetAll()
+        {
+              var features =context.Features.ToList();
+
+            foreach (var feature in features)
+            {
+                if(feature.AdminComments==null)
+                {
+                    feature.AdminComments = "No Comment ";
+                }
+            }
+
+            return features;
+          
+        }
+
+        public IEnumerable<Feature> GetById(string username)
+        {
+            return context.Features.Where(x => x.UserName == username).ToList();
+        }
+    }
+}
